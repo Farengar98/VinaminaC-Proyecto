cef1c84dfad7d14cf1b8756dcaa77e9ca858a199
Unknown <ferranromerogallardo@enti.cat>
2017-10-09 12:16:45 +0200

    lvl1base

---
+++ VitaminaCUnity/Assets/esMouComUnaPalmera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class esMouComUnaPalmera : MonoBehaviour {
+
+    public float moviment;
+
+    // Use this for initialization
+    void Start () {
+        moviment = 0.30f;
+
+    }
+	// Update is called once per frame
+	void Update () {
+        
+
+        
+    }
+}
---
+++ VitaminaCUnity/Assets/playermovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class playermovement : MonoBehaviour {
+
+
+    public float MoveSpeed = 0.1f;
+
+    public float jumpValue = 1;
+    public Rigidbody2D cosa;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(new Vector2(-MoveSpeed, 0));
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(new Vector2(+MoveSpeed, 0));
+        }
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W))
+        {
+            cosa.AddForce(transform.up * jumpValue); 
+        }
+    }
+}
