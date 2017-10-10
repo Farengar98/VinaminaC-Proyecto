9d33f5f8eb4fd9c3b9fae4aef58f22ad9ef7c9bb
Unknown <ferranromerogallardo@enti.cat>
2017-10-10 11:02:32 +0200

    11:02
    los enemigos pillan dirección random

---
+++ VitaminaCUnity/Assets/BasicEnemyAI.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicEnemyAI : MonoBehaviour {
+
+    private bool LeftOrRight;
+    public float enemySpeed;
+    Collision2D collision;
+
+    // Use this for initialization
+    void Start () {
+        LeftOrRight = Random.value > 0.5f;
+        if (LeftOrRight)
+        {
+            transform.localRotation = Quaternion.Euler(0, 180, 0);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (transform.localScale.x < 0)
+        {
+            transform.Translate(new Vector2(enemySpeed/100, 0));
+        }
+        else
+        {
+            transform.Translate(new Vector2(enemySpeed /100, 0));
+        }
+
+        if(collision.gameObject.tag == "Wall")
+        {
+            flip();
+        }
+	}
+
+    void flip()
+    {
+        transform.localRotation = Quaternion.Euler(0, 180, 0);
+    }
+    void OnTriggerEnter2D(Collider2D something)
+    {
+        if (something.tag == "Wall")
+            GetComponent<SpriteRenderer>().flipX = false;
+    }
+
+}
+
--- VitaminaCUnity/Assets/Scripts/esMouComUnaPalmera.cs
+++ VitaminaCUnity/Assets/Scripts/esMouComUnaPalmera.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class esMouComUnaPalmera : MonoBehaviour {
 
-     private Vector3 startPosition;
+     private Vector2 startPosition;
 
     // Use this for initialization
     void Start () {
@@ -14,7 +14,7 @@ public class esMouComUnaPalmera : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-        transform.position = startPosition + new Vector3(Mathf.Sin(Time.time), 0.0f, 0.0f);
+        transform.position = startPosition + new Vector2(Mathf.Sin(Time.time), 0.0f);
 
     }
 }
